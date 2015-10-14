710629144baee17730a527bf39f9d3d1a4e4787c
mw <mw@mw-book>
2015-10-14 21:49:30 +0300

    Syntax cleaned up

--- tsar.sh
+++ tsar.sh
@@ -39,64 +39,64 @@ while getopts "dv" option;do
     esac
 done
 
-LIST=`mktemp`
-LIST_TO_DELETE=`mktemp`
+LIST=$(mktemp)
+LIST_TO_DELETE=$(mktemp)
 FILES_TO_DELETE=0
-DATE_UNIX=`date +%s`
-DATE=`date +%x`
-DAILY_DIFF_SEC=$(($DAILY*86400))
-WEEKLY_DIFF_SEC=$(($WEEKLY*604800))
-MONTHLY_DATE_UNIX=`date +%s -d "$DATE -$MONTHLY months"`
+DATE_UNIX=$(date +%s)
+DATE=$(date +%x)
+DAILY_DIFF_SEC=$((DAILY*86400))
+WEEKLY_DIFF_SEC=$((WEEKLY*604800))
+MONTHLY_DATE_UNIX=$(date +%s -d "$DATE -$MONTHLY months")
 
 if [ $VERBOSE -eq 1 ];then echo "Getting the list of archives from Tarsnap";fi
-$TARSNAP_R --list-archives -v > $LIST
+$TARSNAP_R --list-archives -v > "$LIST"
 
 if [ $? -ne 0 ];then
-    cat $LIST # tarsnap finished with an error, show it
-    shred -u $LIST
+    cat "$LIST" # tarsnap finished with an error, show it
+    shred -u "$LIST"
     exit 3
 fi
 
-while read line;do
-    FILE_DATE=`echo $line | awk '{print $2}'`
-    FILE_NAME=`echo $line | awk '{print $1}'`
-    FILE_DOW=`date +%w -d $FILE_DATE`
-    FILE_DOM=`date +%d -d $FILE_DATE`
-    FILE_UNIX=`date +%s -d $FILE_DATE`
+while read -r line;do
+    FILE_DATE=$(echo "$line" | awk '{print $2}')
+    FILE_NAME=$(echo "$line" | awk '{print $1}')
+    FILE_DOW=$(date +%w -d "$FILE_DATE")
+    FILE_DOM=$(date +%d -d "$FILE_DATE")
+    FILE_UNIX=$(date +%s -d "$FILE_DATE")
     
-    if [ $(($DATE_UNIX-$FILE_UNIX)) -lt $DAILY_DIFF_SEC ];then
+    if [ $((DATE_UNIX-FILE_UNIX)) -lt $DAILY_DIFF_SEC ];then
         if [ $VERBOSE -eq 1 ];then
             echo "File younger than $DAILY days, not touching: $FILE_NAME"
         fi
         continue
-    elif [ $FILE_DOW -eq $DOW ] && [ $(($DATE_UNIX-$FILE_UNIX)) -lt $WEEKLY_DIFF_SEC ];then
+    elif [ "$FILE_DOW" -eq $DOW ] && [ $((DATE_UNIX-FILE_UNIX)) -lt "$WEEKLY_DIFF_SEC" ];then
         if [ $VERBOSE -eq 1 ];then
             echo "File younger than $WEEKLY weeks and its DOW is $FILE_DOW, not touching: $FILE_NAME"
         fi
         continue
-    elif [ $FILE_DOM -eq $DOM ] && [ $MONTHLY_DATE_UNIX -lt $FILE_UNIX ];then
+    elif [ "$FILE_DOM" -eq $DOM ] && [ "$MONTHLY_DATE_UNIX" -lt "$FILE_UNIX" ];then
         if [ $VERBOSE -eq 1 ];then
             echo "File younger than $MONTHLY months and its DOM is $FILE_DOM, not touching: $FILE_NAME"
         fi
         continue
     fi
 
-    echo $line >> $LIST_TO_DELETE
+    echo "$line" >> "$LIST_TO_DELETE"
     ((FILES_TO_DELETE++))
-done < $LIST
+done < "$LIST"
 
 if [ $VERBOSE -eq 1 ];then echo "Total number of $FILES_TO_DELETE files can be deleted.";fi
 
 CMD="$TARSNAP_D -d"
 
 if [ $FILES_TO_DELETE -gt 0 ];then
-    while read line;do
+    while read -r line;do
         CMD="$CMD -f $line"
-    done <$LIST_TO_DELETE
+    done < "$LIST_TO_DELETE"
 
-    if [ $DRY ];then
+    if [ $DRY -eq 1 ];then
         echo "The following command would be executed:"
-        echo $CMD
+        echo "$CMD"
     else
         $CMD
     fi
@@ -105,5 +105,5 @@ else
 fi
 
 if [ $VERBOSE -eq 1 ];then echo "Getting rid of temporary files.";fi
-shred -u $LIST $LIST_TO_DELETE
+shred -u "$LIST" "$LIST_TO_DELETE"
 if [ $VERBOSE -eq 1 ];then echo "Done.";fi
