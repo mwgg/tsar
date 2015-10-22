6826ee35f5a2ed9d55f7b18bb51b94e3f6f2d362
mw <mw@mw-book>
2015-10-22 11:20:35 +0300

    Archive names escaped & more syntax cleanup

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Tsar only manages the cleanup of existing archives, and does so in a simple orga
 
 A few parameters are editable directly within the script, such as the number of daily, weekly and monthly backups to keep, as well as days of the week and days of the month to preserve for weekly and monthly backups. As Tsar runs the `tarsnap` command twice - once to obtain the list of archives, and again to delete them as necessary - different commands may be specified for each execution. This is handy when different keys or configuration files and options are used for different tarsnap tasks.
 
-Once configured, Tsar may be executed without any parameters. A couple of flags are available, however::
+Once configured, Tsar may be executed without any parameters. A couple of flags are available, however:
 ```
 -d   Dry run. Will show the delete command instead of running it.
 -v   Verbose output. Will produce commentary to the progress of the script, as well as logic behind keeping the files that need not be deleted.
--- tsar.sh
+++ tsar.sh
@@ -48,7 +48,7 @@ DAILY_DIFF_SEC=$((DAILY*86400))
 WEEKLY_DIFF_SEC=$((WEEKLY*604800))
 MONTHLY_DATE_UNIX=$(date +%s -d "$DATE -$MONTHLY months")
 
-if [ $VERBOSE -eq 1 ];then echo "Getting the list of archives from Tarsnap";fi
+if [ "$VERBOSE" -eq 1 ];then echo "Getting the list of archives from Tarsnap";fi
 $TARSNAP_R --list-archives -v > "$LIST"
 
 if [ $? -ne 0 ];then
@@ -64,18 +64,18 @@ while read -r line;do
     FILE_DOM=$(date +%d -d "$FILE_DATE")
     FILE_UNIX=$(date +%s -d "$FILE_DATE")
     
-    if [ $((DATE_UNIX-FILE_UNIX)) -lt $DAILY_DIFF_SEC ];then
-        if [ $VERBOSE -eq 1 ];then
+    if [ $((DATE_UNIX-FILE_UNIX)) -lt "$DAILY_DIFF_SEC" ];then
+        if [ "$VERBOSE" -eq 1 ];then
             echo "File younger than $DAILY days, not touching: $FILE_NAME"
         fi
         continue
-    elif [ "$FILE_DOW" -eq $DOW ] && [ $((DATE_UNIX-FILE_UNIX)) -lt "$WEEKLY_DIFF_SEC" ];then
-        if [ $VERBOSE -eq 1 ];then
+    elif [ "$FILE_DOW" -eq "$DOW" ] && [ $((DATE_UNIX-FILE_UNIX)) -lt "$WEEKLY_DIFF_SEC" ];then
+        if [ "$VERBOSE" -eq 1 ];then
             echo "File younger than $WEEKLY weeks and its DOW is $FILE_DOW, not touching: $FILE_NAME"
         fi
         continue
-    elif [ "$FILE_DOM" -eq $DOM ] && [ "$MONTHLY_DATE_UNIX" -lt "$FILE_UNIX" ];then
-        if [ $VERBOSE -eq 1 ];then
+    elif [ "$FILE_DOM" -eq "$DOM" ] && [ "$MONTHLY_DATE_UNIX" -lt "$FILE_UNIX" ];then
+        if [ "$VERBOSE" -eq 1 ];then
             echo "File younger than $MONTHLY months and its DOM is $FILE_DOM, not touching: $FILE_NAME"
         fi
         continue
@@ -85,16 +85,16 @@ while read -r line;do
     ((FILES_TO_DELETE++))
 done < "$LIST"
 
-if [ $VERBOSE -eq 1 ];then echo "Total number of $FILES_TO_DELETE files can be deleted.";fi
+if [ "$VERBOSE" -eq 1 ];then echo "Total number of $FILES_TO_DELETE files can be deleted.";fi
 
 CMD="$TARSNAP_D -d"
 
-if [ $FILES_TO_DELETE -gt 0 ];then
+if [ "$FILES_TO_DELETE" -gt 0 ];then
     while read -r line;do
-        CMD="$CMD -f $line"
+        CMD="$CMD -f $(printf "%q" "$line")"
     done < "$LIST_TO_DELETE"
 
-    if [ $DRY -eq 1 ];then
+    if [ "$DRY" -eq 1 ];then
         echo "The following command would be executed:"
         echo "$CMD"
     else
@@ -104,6 +104,6 @@ else
     echo "Nothing to do."
 fi
 
-if [ $VERBOSE -eq 1 ];then echo "Getting rid of temporary files.";fi
+if [ "$VERBOSE" -eq 1 ];then echo "Getting rid of temporary files.";fi
 shred -u "$LIST" "$LIST_TO_DELETE"
-if [ $VERBOSE -eq 1 ];then echo "Done.";fi
+if [ "$VERBOSE" -eq 1 ];then echo "Done.";fi
